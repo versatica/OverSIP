6dfe11fb0c964cabd690099ee90dae9563d7f7cc
Iñaki Baz Castillo <ibc@aliax.net>
2012-08-19 23:54:41 +0200

    Added EM::Connection#open? method. Version 1.1.0.beta5.

--- lib/oversip/version.rb
+++ lib/oversip/version.rb
@@ -6,7 +6,7 @@ module OverSIP
     MAJOR = 1
     MINOR = 1
     TINY  = 0
-    DEVEL = "beta4"  # Set to nil for stable releases.
+    DEVEL = "beta5"  # Set to nil for stable releases.
   end
 
   PROGRAM_NAME     = "OverSIP"
