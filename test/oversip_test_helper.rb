4a299b2e987a45486ff2ca4533b15dc182a00aa3
Iñaki Baz Castillo <ibc@aliax.net>
2012-08-28 15:24:31 +0200

    Test helper improved.

--- test/oversip_test_helper.rb
+++ test/oversip_test_helper.rb
@@ -6,11 +6,11 @@ require "oversip/master_process"
 class OverSIPTest < Test::Unit::TestCase
 
   def assert_true(object, message="")
-    assert_equal(object, true, message)
+    assert_equal(true, object, message)
   end
 
   def assert_false(object, message="")
-    assert_equal(object, false, message)
+    assert_equal(false, object, message)
   end
 
   def assert_equal_options(options, element)
