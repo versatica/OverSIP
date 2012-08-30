e771d5d45eb368a3c28ffb43a06b6e3f23e7a047
Iñaki Baz Castillo <ibc@aliax.net>
2012-08-30 13:30:43 +0200

    Improvements in OverSIP::Utils module methods.

--- lib/oversip/utils.rb
+++ lib/oversip/utils.rb
@@ -7,7 +7,7 @@ module OverSIP
     # For example in Ruby the following returns false:
     #   "iñaki".force_encoding(::Encoding::BINARY) == "iñaki"
     def self.string_compare string1, string2
-      string1.force_encoding(::Encoding::BINARY) == string2.force_encoding(::Encoding::BINARY)
+      string1.to_s.force_encoding(::Encoding::BINARY) == string2.to_s.force_encoding(::Encoding::BINARY)
     end
 
     # This avoid "invalid byte sequence in UTF-8" when the directly doing:
@@ -16,7 +16,8 @@ module OverSIP
     # NOTE: expression argument must be a Regexp expression (with / symbols at the
     # begining and at the end).
     def self.regexp_compare string, expression
-      return false  unless string && string.valid_encoding?
+      string = string.to_s
+      return false  unless string.valid_encoding?
       string.force_encoding(::Encoding::BINARY) =~ expression
     end
 
